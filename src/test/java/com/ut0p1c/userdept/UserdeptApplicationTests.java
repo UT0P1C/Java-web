c24c1a21c55ca939fbf5c56e4a6276f5192311a1
UT0P1C <ljonham@gmail.com>
2023-03-30 20:03:59 -0300

    first commit

---
+++ src/test/java/com/ut0p1c/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ut0p1c.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
